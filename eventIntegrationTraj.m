75151074e8abe127a944a42385368871a0223ffc
KirillRnd <kirill_rnd@mail.ru>
2020-04-11 18:17:30 +0300

    Regularization. Passive movement

---
+++ eventIntegrationTraj.m
@@ -0,0 +1,26 @@
+function [value, isterminal, direction] = eventIntegrationTraj(tau, y, angle)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+u=y(1:2);
+v=y(3:4);
+h=y(5);
+L = [[u(1) -u(2)];
+    [u(2) u(1)]]; 
+
+r=[(L*u)' 0];
+b=[cos(angle) sin(angle) 0];
+xa = r(1);
+ya = r(2);
+xb = b(1);
+yb = b(2);
+an = pi/2*((1+sign(xa))*(1-sign(ya^2))-(1+sign(xb))*(1-sign(yb^2)))...
+    +pi/4*((2+sign(xa))*sign(ya)-(2+sign(xb))*sign(yb))...
+    +sign(xa*ya)*atan((abs(xa)-abs(ya))/(abs(xa)+abs(ya)))...
+    -sign(xb*yb)*atan((abs(xb)-abs(yb))/(abs(xb)+abs(yb)));
+%an = atan2(norm(cross(r,b)), dot(r,b));
+
+value = an;
+isterminal = 1;
+direction = 0;
+end
+
--- findOptimum.m
+++ findOptimum.m
@@ -1,53 +1,39 @@
 
 mug = 132712.43994*(10^6)*(10^(3*3));
 ae = 149597870700;
- y0 = cat(2,[1*ae 0 0],[0 (mug/(1*ae))^(1/2) 0],[0 0 0],[0 0 0],...
-    reshape(zeros(3),[1,9]), reshape(zeros(3),[1,9]),...
-    reshape(eye(3),[1,9]), reshape(zeros(3),[1,9]),...
-    reshape(zeros(3),[1,9]), reshape(zeros(3),[1,9]),...
-    reshape(zeros(3),[1,9]), reshape(eye(3),[1,9])...
-    )';
+
+r0 = [1*ae 0 ]';
+V0 = [0 (mug/(1*ae))^(1/2) ]';
+
+u0 = [0 0]';
+h0=(norm(V0)^2)/2-mug/norm(r0);
+
+u0(2) = sqrt((norm(r0)-r0(1))/2);
+u0(1) = sqrt(norm(r0)-u0(2)^2);
+
+L = [[u0(1) -u0(2)];
+    [u0(2) u0(1)]];  
+v0 = L'*V0/(2*sqrt(-2*h0));
+
+y0 = cat(1, u0, v0, h0)';
 %Определяем tf
 T=2*pi*sqrt((1*ae)^3/mug);
-tf=9*T/12;
-optionsInn = odeset('AbsTol',1e-12);
+%tf=3*T/12;
+tf = 10;
+angle = 3*pi/2;
 
-[t,y] = ode45(@(t,y) integrateTraectory(t,y,mug),[0 tf],y0,optionsInn);
-plot(y(:,1),y(:,2));
+options = odeset('Events', @(t, y) eventIntegrationTraj(t, y, angle));
+options = odeset(options,'AbsTol',1e-10);
+options = odeset(options,'RelTol',1e-10);
+
+[t,y] = ode45(@(t,y) integrateTraectory(t,y,mug),[0 tf],y0, options);
+u = y(:, 1:2);
+r=zeros(length(u),2);
+for i = 1:length(u)
+    x = u(i,:);
+    L = [[x(1) -x(2)];
+    [x(2) x(1)]];
+    r(i,:)=L*x';
+end
+plot(r(:, 1), r(:, 2))
 axis equal
-drdzdt=reshape(y(end,49:66),[3,6]);
-drdz=reshape(y(end,13:30),[3,6]);
-
-dfdz = cat(1,drdz,drdzdt);
-
-th_mars = pi / 6;
-
-rf = [1.52*ae*cos(th_mars) 1.52*ae*sin(th_mars) 0];
-vf = [(mug/(1.5*ae))^(1/2)*cos(th_mars+pi/2) (mug/(1.5*ae))^(1/2)*sin(th_mars+pi/2) 0];
-
-b=y(end,1:6)-cat(2,rf,vf);
-%оптимизируем траекторию
-z0=[0 0 0 0 0 0];
-
-optionsExt = odeset('AbsTol',1e-16);
-
-[tau,z] = ode45(@(t,y) optimiseToMars(t,y,b,tf),[0 1],z0, optionsExt);
-
-y0 = cat(2,[1*ae 0 0],[0 (mug/(1*ae))^(1/2) 0],z(end,:),...
-    reshape(zeros(3),[1,9]), reshape(zeros(3),[1,9]),...
-    reshape(eye(3),[1,9]), reshape(zeros(3),[1,9]),...
-    reshape(zeros(3),[1,9]), reshape(zeros(3),[1,9]),...
-    reshape(zeros(3),[1,9]), reshape(eye(3),[1,9])...
-    )';
-[t,y] = ode45(@(t,y) integrateTraectory(t,y,mug),[0 tf],y0,optionsInn);
-plot(y(:,1),y(:,2));
-axis equal
-hold on
-th = 0:pi/50:2*pi;
-plot(ae*cos(th),ae*sin(th));
-plot(1.5*ae*cos(th),1.5*ae*sin(th));
-plot(rf(1),rf(2),'b--o')
-plot(ae, 0,'b--o')
-plot(0, 0,'y--o')
-plot(rf(1)+b(1),rf(2)+b(2),'r--o')
-hold off;
