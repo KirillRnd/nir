477f801ca194df1b8796e13771462a82a6a7245f
KirillRnd <kirill_rnd@mail.ru>
2020-11-22 16:21:55 +0300

    results

--- fminconSearch.m
+++ fminconSearch.m
@@ -53,16 +53,16 @@ lb(11) = s_a;
 ub(11) = s_b;
 %домножаем на коэффициент 1е-12, чтобы fmincon работал с более крупными
 %величинами и не выдавал лишних ворнингов
-
+tic;
 fun=@(x)fun2min([x(1:10)*modifier_p x(11)], case_traj, t_Mars_0);
 
 options = optimoptions('fmincon','UseParallel', true);
 options = optimoptions(options, 'Display', 'iter');
-options = optimoptions(options, 'OptimalityTolerance', 1e-6);
+options = optimoptions(options, 'OptimalityTolerance', 1e-10);
 %options = optimoptions(options, 'Algorithm', 'sqp');
 
 x = fmincon(fun, x0, A, b, Aeq, beq, lb, ub,[], options)
-
+toc
 px = x(1:10)*modifier_p;
 s_f = x(11);
 %задаем начальные условия
@@ -141,7 +141,7 @@ box off;
 set(gca,'FontSize',14)
 
 figure(3);
-plot(t/(24*3600), s);
+plot(t/(24*3600), s, 'LineWidth', 3);
 title('Зависимость мнимого времени от физического')
 xlabel('t, время, дни')
 ylabel('s, мнимое время')
@@ -150,7 +150,7 @@ set(gca,'FontSize',14)
 
 figure(4);
 m=massLP(Jt, m0, N);
-plot(t/(24*3600), m);
+plot(t/(24*3600), m, 'LineWidth', 3);
 title('Зависимость массы от времени')
 xlabel('t, время, дни')
 ylabel('m, масса, кг')
@@ -165,7 +165,7 @@ hold on;
 th = 0:pi/50:2*pi;
 plot(cos(th),sin(th),'k');
 plot(1.52*cos(th),1.52*sin(th),'r');
-plot(rr(:, 1), rr(:, 2),'b', 'LineWidth', 1.5)
+plot(rr(:, 1), rr(:, 2),'b', 'LineWidth', 2.5)
 a_scale=3e-01/mean(vecnorm(a, 2, 2));
 %a_scale=0;
 d = 24*3600;
@@ -190,3 +190,9 @@ ax.XAxisLocation = 'origin';
 ax.YAxisLocation = 'origin';
 box off;
 hold off;
+
+m(1)-m(end)
+r_Mars=[1.52*ae*cos(angle_M); 1.52*ae*sin(angle_M)]';
+V_Mars=((mug_0/(1.52*ae))^(1/2))*[cos(angle_M+pi/2) sin(angle_M+pi/2)];
+norm(rr(end, 1:2)*r_norm-r_Mars)
+norm(VV(end, 1:2)*V_norm-V_Mars)
